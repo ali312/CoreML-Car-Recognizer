263942b9f52d6d7dca9758e636a9b4c28940a7f9
Anton Kuznetsov <a.kuznetsov@crystalnix.com>
2018-01-06 19:54:02 +0600

    Initial Commit

---
+++ CoreML Car Recognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML Car Recognizer
+//
+//  Created by Anton Kuznetsov on 06.01.18.
+//  Copyright © 2018 Anton Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
